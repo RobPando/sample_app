2e5bee535833aab719f0ffc29051aac8dbdc867c
Roberto Pando <roberto_pando@hotmail.com>
2016-11-02 14:16:10 +0100

    Implement advanced login

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserSignupTest < ActionDispatch::IntegrationTest
 
-  test "invalid signup informatio" do
+  test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name: "",
