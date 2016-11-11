0dc8b466fae6545b48c9cf75b638fdebd65f7bbb
Roberto Pando <roberto_pando@hotmail.com>
2016-11-11 13:50:49 +0100

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -68,6 +68,17 @@ class UserTest < ActiveSupport::TestCase
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
   end
+
+  test "should follow and unfollow a user" do
+    roberto = users(:roberto)
+    archer = users(:archer)
+    assert_not roberto.following?(archer)
+    roberto.follow(archer)
+    assert roberto.following?(archer)
+    assert archer.followers.include?(roberto)
+    roberto.unfollow(archer)
+    assert_not roberto.following?(archer)
+  end
 end
 
 
